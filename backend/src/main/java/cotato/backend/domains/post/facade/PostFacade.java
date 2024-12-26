e9352815f19b6bddf8710871aa30a11e097bbd4b
chyun <och2510@gmail.com>
2024-12-26 23:48:13 +0900

    refactor: 게시물 조회수 동시성 문제 해결
    - synchronized 키워드 + facade 패턴 적용

---
+++ backend/src/main/java/cotato/backend/domains/post/facade/PostFacade.java
@@ -0,0 +1,36 @@
+package cotato.backend.domains.post.facade;
+
+import cotato.backend.domains.post.dto.PostDTO;
+import cotato.backend.domains.post.dto.PostListDTO;
+import cotato.backend.domains.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostFacade {
+    private final PostService postService;
+
+
+    public void saveEstatesByExcel(final String filePath) {
+        postService.saveEstatesByExcel(filePath);
+    }
+
+
+    public void savePost(final PostDTO postDTO) {
+        postService.savePost(postDTO);
+    }
+
+
+    public synchronized PostDTO findPostById(final long id) {
+        return postService.findPostById(id);
+    }
+
+    public void deletePostById(final long id) {
+        postService.deletePostById(id);
+    }
+
+    public PostListDTO getPosts(final int page) {
+        return postService.getPosts(page);
+    }
+}
